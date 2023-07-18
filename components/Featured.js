c4cfbddfefed828f397ee76da7705daa7a5582e6
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-07-18 17:56:02 +0530

    Update Featured.js

--- components/Featured.js
+++ components/Featured.js
@@ -21,19 +21,7 @@ const Featured = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.arrowContainer} style={{ left: 0 }} onClick={() => handleArrow("l")}>
-                <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain" />
-            </div>
-            <div className={styles.wrapper} style={{ transform: `translateX(${-100 * index}vw)` }}>
-                {images.map((img, i) => (
-                    <div className={styles.imgContainer} key={i}>
-                        <Image src={img} alt="" layout="fill" objectFit="contain" />
-                    </div>
-                ))}
-            </div>
-            <div className={styles.arrowContainer} style={{ right: 0 }} onClick={() => handleArrow("r")}>
-                <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain" />
-            </div>
+            Hello gfhjsd ufewihjsk ugefhjdksn uiewfh uewf o ufwehj owehfin oehfiw nkl  eiwfohfk hoiewf  oihewf oehfw 
         </div>
     );
 };
