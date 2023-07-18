60dee36060a717278c010c2a4baa09f2a068d291
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-07-18 18:35:12 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,10 +6,8 @@ const handler = (req, res) => {
         const { username, password } = req.body;
         if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
             res.setHeader("Set-Cookie", cookie.serialize("token", process.env.TOKEN, {
-                httpOnly: true,
-                secure: true,
-                maxAge: 60 * 60,
                 sameSite: "strict",
+                maxAge: 60 * 60,
                 path: '/'
             }))
             res.status(200).json("Successfull");
